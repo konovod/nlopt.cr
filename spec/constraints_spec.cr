d92417bbe336effd91cd76aac94305569d994309
Konovod <konovod@gmail.com>
2017-07-18 22:44:35 +0000

    equality spec

--- spec/constraints_spec.cr
+++ spec/constraints_spec.cr
@@ -32,4 +32,35 @@ describe NLopt do
     x[1].should be_close(0.296296, 1e-5)
     f.should be_close(0.544330847, 1e-5)
   end
+
+  it "can work with equalities" do
+    s1 = NLopt::Solver.new(NLopt::Algorithm::LnCobyla, 3)
+    s1.xtol_rel = 1e-8
+    s1.objective = ->(x : Slice(Float64), grad : Slice(Float64)?) do
+      if grad
+        grad[0] = -x[1]*x[2]
+        grad[1] = -x[0]*x[2]
+        grad[2] = -x[0]*x[1]
+      end
+      -x[0]*x[1]*x[2]
+    end
+    s1.constraints << NLopt::SingleConstraint.new(1e-8, equality: true) do |x, grad|
+      if grad
+        grad[0] = 1.0
+        grad[1] = 1.0
+        grad[2] = 1.0
+      end
+      x[0] + x[1] + x[2] - 3
+    end
+
+    s1.variables[0].set(min: 0.0, guess: 2.5)
+    s1.variables[1].set(guess: 0.5)
+    s1.variables[2].set(min: 0.0, guess: 0.0)
+    res, x, f = s1.solve
+    res.should eq NLopt::Result::XtolReached
+    x[0].should be_close(1, 1e-7)
+    x[1].should be_close(1, 1e-7)
+    x[2].should be_close(1, 1e-7)
+    f.should be_close(-1, 1e-8)
+  end
 end
